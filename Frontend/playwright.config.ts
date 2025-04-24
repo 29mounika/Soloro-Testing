bd7e1b9ab144958e69e305e5faa512ba231a2d87
Mounika b <mounika.b@alpha-numero.com>
2025-04-24 15:14:16 +0530

    canges

--- Frontend/playwright.config.ts
+++ Frontend/playwright.config.ts
@@ -1,10 +1,10 @@
-
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    video: 'on', // Change to 'on' to always record
+    video: 'on', // Always record video
   },
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
-  testDir: './Frontend', // Optional: where you put tests
+  testDir: '.', // Root of Frontend folder
 });
+
